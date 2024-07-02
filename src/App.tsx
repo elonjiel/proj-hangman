0540994457d6aa8e985477d2a3e8919a44c353b3
Elon Jiel <elonudemy@gmail.com>
2024-07-02 17:31:11 +0800

    Added Hangman Drawing and Word

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import words from "./wordList.json"
+import { HangmanDrawing } from "./HangmanDrawing"
+import { HangmanWord } from "./HangmanWord"
+import { Keyboard } from "./Keyboard"
+
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)]
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [wordToGuess, setWordToGuess] = useState(getWord)
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  console.log(wordToGuess)
   return (
-    <>
-      <h1>Hangman</h1>
-    </>
+      <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+      >
+        <div style={{ fontSize: "2rem", textAlign: "center" }}>
+        Win Lose
+       </div>
+       <HangmanDrawing />
+       <HangmanWord />
+       <Keyboard />
+      </div>
   )
 }
 
---
+++ src/HangmanWord.tsx
@@ -0,0 +1,21 @@
+export function HangmanWord() {
+    const word = "test"
+    return (
+      <div
+        style={{
+          display: "flex",
+          gap: ".25em",
+          fontSize: "6rem",
+          fontWeight: "bold",
+          textTransform: "uppercase",
+          fontFamily: "monospace",
+        }}
+      >
+        {word.split("").map((letter, index) => (
+            <span style={{borderBottom: ".1em solid black"}}>
+                <span>{letter}</span>
+            </span>
+        ))}
+      </div>
+    )
+}
